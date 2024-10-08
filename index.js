9217253e9e7d733aa9ee6506a57d22c91265a5c0
AbdulrahmanSayed99 <abdelrhmanelbrolsy@yahoo.com>
2024-10-09 02:39:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let multer = require('multer');
 
+app.post('/api/fileanalyse', multer().single('upfile'), ((req, res)=>{
+  res.json({'name': req.file.originalname,
+    'type': req.file.mimetype,
+    'size': req.file.size
+  })
+}))
 
 
 const port = process.env.PORT || 3000;
